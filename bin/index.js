ff83fe104bf1b4fd0149a06e7eb3bd26c4f63e49
Adrian Kröger <a.kroeger@grimme.de>
2022-05-02 11:12:59 +0200

    feat(parser): added formula parser for calculation

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,44 @@
 #!/usr/bin/env node
+const Excel = require('exceljs');
+const FormulaParser = require('hot-formula-parser').Parser;
+const parser = new FormulaParser();
 
-console.log("Hello World");
+
+const excelUpdate = async (file, worksheetId, rowId, columnLabel, value) => {
+   const workbook = new Excel.Workbook();
+   await workbook.xlsx.readFile(file);
+
+   const worksheet = workbook.getWorksheet(worksheetId);
+
+   let inputRow = worksheet.getRow(rowId);
+   inputRow.getCell(columnLabel).value = value;
+   inputRow.commit();
+
+   return worksheet;
+}
+
+const excelRead = async (worksheet, cellLabel) => {
+   if (worksheet.getCell(cellLabel).formula) {
+      return parser.parse(worksheet.getCell(cellLabel).formula).result;
+    } else {
+      return worksheet.getCell(cellCoord.label).value;
+    }
+};
+
+const exec = async () => {
+   parser.on('callCellValue', function(cellCoord, done) {
+      if (worksheet.getCell(cellCoord.label).formula) {
+        done(parser.parse(worksheet.getCell(cellCoord.label).formula).result);
+      } else {
+        done(worksheet.getCell(cellCoord.label).value);
+      }
+    });
+
+   const worksheet = await excelUpdate("ExcelLogic.xlsx", "Tabelle1", 3, 'B', 5);
+
+   const result = await excelRead(worksheet, "B5");
+
+   console.log(result);
+};
+
+exec();
